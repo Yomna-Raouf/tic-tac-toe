ea1aaeb6223059929df0b3bcb5d7dceaa279fd4d
yomna raouf <yomnaraouf99@gmail.com>
2020-06-01 00:30:42 -0700

    taking turns

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class Board extends React.Component {
 
  handleClick(i) {
   const squares = this.state.squares.slice();
-  squares[i] = 'X';
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
   this.setState({
     squares: squares,
     xIsNext: !this.state.xIsNext,
@@ -46,7 +46,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
